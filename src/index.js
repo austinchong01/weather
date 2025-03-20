493b92bc76eabbb140a732227dacab0cb56522c4
Austin <austin.inhyuk.chong@gmail.com>
2025-03-19 18:08:34 -0700

    Implement weather API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import "./styles.css";
 
-console.log("Hello World");
+const input = document.querySelector("#location");
+const searchBtn = document.querySelector("#search");
+
+function search() {
+  event.preventDefault();
+  const location = input.value;
+
+  fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&elements=datetime%2CdatetimeEpoch%2Cname%2Caddress%2Ctemp&key=KT8X949TC3E7E5KJVX5B3YJSL&contentType=json`,
+    { mode: "cors" },
+  )
+    .then((response) => response.json())
+    .then((response) => response.days[0])
+    .catch((error) => {
+      console.log(error);
+    });
+
+  input.value = "";
+}
+
+searchBtn.addEventListener("click", search);
