a4e6253273441a6ca4e4afd12b905963dfb85a35
Austin <austin.inhyuk.chong@gmail.com>
2025-03-20 11:26:59 -0700

    Add Weather to DOM

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
 import "./styles.css";
 
-async function processData(location) {
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&elements=datetime%2Cname%2Ctemp%2Cprecipcover%2Cpreciptype%2Ccloudcover%2Cconditions&include=current&key=KT8X949TC3E7E5KJVX5B3YJSL&contentType=json`,
-    { mode: "cors" },
-  );
-  const responseJSON = await response.json();
-  const { currentConditions } = responseJSON;
+function clearDOM(time, temperature, cloud, precipitation) {
+  time.innerHTML = "";
+  temperature.innerHTML = "";
+  cloud.innerHTML = "";
+  precipitation.innerHTML = "";
+}
 
-  console.log(currentConditions);
+function renderDOM(currentConditions) {
+  const content = document.querySelector("#content");
+
+  const time = document.querySelector("#time");
+  const temperature = document.querySelector("#temperature");
+  const cloud = document.querySelector("#cloud");
+  const precipitation = document.querySelector("#precipitation");
+
+  clearDOM(time, temperature, cloud, precipitation);
+
+  time.innerHTML = `Time: ${currentConditions.datetime}`;
+  temperature.innerHTML = `Temperature: ${currentConditions.temp}`;
+  cloud.innerHTML = `Cloud Status: ${currentConditions.conditions}`;
+  if (currentConditions.preciptype === null) {
+    precipitation.innerHTML = "Precipitation: None";
+  } else {
+    precipitation.innerHTML = `Precipitation: ${currentConditions.preciptype}`;
+  }
+}
+
+async function processData(location) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&elements=datetime%2Cname%2Ctemp%2Cprecipcover%2Cpreciptype%2Ccloudcover%2Cconditions&include=current&key=KT8X949TC3E7E5KJVX5B3YJSL&contentType=json`,
+      { mode: "cors" },
+    );
+    const responseJSON = await response.json();
+    const { currentConditions } = responseJSON;
+
+    renderDOM(currentConditions);
+    console.log(currentConditions);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function search() {
